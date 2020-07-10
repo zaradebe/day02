86ee08d936779853bc5214e26bc793d4cd4db5cd
Zanele Radebe <zaradebe@c4r24s5.wethinkcode.co.za>
2020-07-10 10:46:45 +0200

    Display different combinations in ascending order

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -2,5 +2,31 @@
 
 void ft_print_comb(void)
 { 
+ char a;
+ char b;
+ char c;
+
+  a='0';
+  b='0';
+  c='0';
+
+  while (a<'9'){
+	  while(b<'9'){
+	 
+		 while(c<'9'){
+		     ft_putchar(a);
+		     ft_putchar(b);
+			 ft_putchar(c);
+             ft_putchar(',');
+			 ft_purchar(' ');
+			 c++;
+		 }
+		 c = '0';
+		 b++;
+	  }
+	 
+   b = '0';
+   a++;
+  }
 
 }
