5abdd4189fe54df23221799f36073e2a1f1d3c4a
Zanele Radebe <zaradebe@c4r24s5.wethinkcode.co.za>
2020-07-10 11:07:45 +0200

    Combinations in ascending order

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -14,12 +14,15 @@ void ft_print_comb(void)
 	  while(b<'9'){
 	 
 		 while(c<'9'){
+			 while(a<b && b<c){
 		     ft_putchar(a);
 		     ft_putchar(b);
 			 ft_putchar(c);
              ft_putchar(',');
 			 ft_purchar(' ');
+			 }
 			 c++;
+			
 		 }
 		 c = '0';
 		 b++;
